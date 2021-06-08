138396f1676e207f00a991230190033c7f7c9ca4
unknown <pokemaster.2201@gmail.com>
2021-06-07 22:54:06 -0400

    Bastian 07-06: Realice el inner page para ser utilizado en todos los templates, correciones en index y cambios en urls y views

--- mesta/arriendo/views.py
+++ mesta/arriendo/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth import login, authenticate
+from django.shortcuts import get_object_or_404,render,HttpResponseRedirect
 
 # Create your views here.
 def index(request):
@@ -7,3 +10,9 @@ def index(request):
         request,
         'index.html',
         )
+
+def login(request):
+	return render(request, 'login.html') 
+
+def estacionamientos(request):
+	return render(request, 'estacionamientos.html')    
